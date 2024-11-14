659bee1a57c7eb7bfdf408d2ad0f834df907070b
furkanqq <133240374+furkanqq@users.noreply.github.com>
2024-11-14 17:51:58 +0300

    size image

--- src/assets/image.ts
+++ src/assets/image.ts
@@ -1,13 +1,13 @@
 import Foto from './image/1684620138660.jpeg';
-import DTK from './image/denizcilikTahkim.png';
-import Binance from './image/binance.png';
-import Blog from './image/blogger.png';
-import Solution from './image/solution.png';
-import NPM from './image/npm.png';
-import GL from './image/gl.png';
-import MV from './image/mv.png';
-import ART from './image/art.png';
-import BLN from './image/bln.png';
+import DTK from './image/denizcilikTahkim.webp';
+import Binance from './image/binance.webp';
+import Blog from './image/blogger.webp';
+import Solution from './image/solution.webp';
+import NPM from './image/npm.webp';
+import GL from './image/gl.webp';
+import MV from './image/mv.webp';
+import ART from './image/art.webp';
+import BLN from './image/bln.webp';
 
 type ImagesType = {
   [key: string]: any;
