03d7b45f9b9fa3a838c1aac83547391eb4171086
furkanqq <133240374+furkanqq@users.noreply.github.com>
2025-04-01 10:39:36 +0300

    some image fix

--- src/assets/image.ts
+++ src/assets/image.ts
@@ -1,18 +1,18 @@
 import Foto from './image/1684620138660.jpeg';
-import DTK from './image/denizcilikTahkim.webp';
-import Binance from './image/binance.webp';
-import Blog from './image/blogger.webp';
-import Solution from './image/solution.webp';
-import NPM from './image/npm.webp';
-import GL from './image/gl.webp';
-import MV from './image/mv.webp';
-import ART from './image/art.webp';
-import BLN from './image/bln.webp';
-import Blind from './image/blindlover.webp';
+// import DTK from './image/denizcilikTahkim.webp';
+// import Binance from './image/binance.webp';
+// import Blog from './image/blogger.webp';
+// import Solution from './image/solution.webp';
+// import NPM from './image/npm.webp';
+// import GL from './image/gl.webp';
+// import MV from './image/mv.webp';
+// import ART from './image/art.webp';
+// import BLN from './image/bln.webp';
+// import Blind from './image/blindlover.webp';
 import Love from './image/love.jpg';
 import Babe from './image/babe.jpeg';
-import Momey from './image/momey.webp';
-import FHTS from './image/fithouse.webp';
+// import Momey from './image/momey.webp';
+// import FHTS from './image/fithouse.webp';
 
 type ImagesType = {
   [key: string]: any;
@@ -20,20 +20,20 @@ type ImagesType = {
 
 const Images: ImagesType = {
   Foto,
-  DTK,
-  Binance,
-  Blog,
-  Solution,
-  BLN,
-  NPM,
-  GL,
-  MV,
-  ART,
-  Blind,
   Love,
   Babe,
-  Momey,
-  FHTS,
+  // DTK,
+  // Binance,
+  // Blog,
+  // Solution,
+  // BLN,
+  // NPM,
+  // GL,
+  // MV,
+  // ART,
+  // Blind,
+  // Momey,
+  // FHTS,
 };
 
 export default Images;
